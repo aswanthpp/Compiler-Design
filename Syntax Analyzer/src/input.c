e3953b9c403b68bf08c36429756dd9b7fa928341
ASWANTH P P <ppaswanth3@gmail.com>
2018-02-12 15:50:39 +0530

    Update input.c

--- Syntax Analyzer/src/input.c
+++ Syntax Analyzer/src/input.c
@@ -2,14 +2,13 @@
 void sum(int ,int);
 void main()
 {
- 	int a=5,b;
+ 	int a=5;
+ 	char b;
  	printf("\nEnter Number:");
  	scanf("%d",&b);
  	sum(5,b);
- 	
- 	
 }
-void sum(int inp1,int inp2){
+void sum(int inp1,char inp2){
 	int s=0;
 	s=inp1+inp2;
 	printf("\nSum : %d",s);
